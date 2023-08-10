86d9d8f399c748d5cfec22bacc182e8c30eaf87c
suryaharanadh08 <141898609+suryaharanadh08@users.noreply.github.com>
2023-08-10 12:50:37 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,9 @@
-print("welcome to python")
+Mkdir name 
+Cd name 
+Git init
+Add a file 
+Git add file 
+Git status 
+Git commit -m "added successfully" 
+Git remote add origin path
+Git push - - set origin master 
